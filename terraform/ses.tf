dda009a3ad0024064e31b739bbf97f0983183f04
Anjishnu Ganguly <skythrill256@Anjishnus-MacBook-Pro.local>
2025-12-29 16:45:25 +0530

    feat: Newest structure

--- terraform/ses.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ses_email_identity" "verified" {
-  email = var.ses_verified_email
-}
-
-output "ses_verified_email" {
-  value       = aws_ses_email_identity.verified.email
-  description = "SES verified email identity"
-}
